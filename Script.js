0a7222fa804a40401e19611c087b6b0cac1318e2
Anubis-vera <frz.lunarwolf@gmail.com>
2026-01-11 00:03:15 -0600

    Add video playback controls and event listener

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
+const vid = document.getElementById("heroVid");
 
+vid.muted = false;
+vid.loop = false;
+vid.autoplay = true;
+vid.play();
+
+vid.addEventListener("ended", () => {
+  vid.muted = true;
+  vid.loop = true;
+  vid.play();
+});
